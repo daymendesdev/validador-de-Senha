9f333fc1e2f1a889eeda7ff429c778eab1857ad5
Dayanne Mendes <daymendesdev@gmail.com>
2024-10-29 17:46:49 -0300

    Validação de senha

---
+++ validar.js
@@ -0,0 +1,15 @@
+function validarSenha(event) {
+    event.preventDefault();
+
+    const senha = document.getElementById("password");
+    const senha2 = document.getElementById("confirmPassword");
+    const exibirErro = document.getElementById("exibirErro");
+
+    if (senha.value === senha2.value) {
+        window.location.href = "opcoes.html";
+    } else {
+        document.getElementById("exibirErro").innerText = "As senhas devem ser iguais!";
+        document.getElementById("exibirErro").style.color = "red";
+    }
+}
+formCadastro.addEventListener("submit", validarSenha);
