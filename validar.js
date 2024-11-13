e5df8849f77d30caeab8b7187a7e64e2d879c766
daymendesdev <daymendesdev@gmail.com>
2024-11-13 17:52:18 -0300

    inclusão da verificação do email

--- validar.js
+++ validar.js
@@ -4,6 +4,14 @@ function validarSenha(event) {
     const senha = document.getElementById("password");
     const senha2 = document.getElementById("confirmPassword");
     const exibirErro = document.getElementById("exibirErro");
+    const email = document.getElementById("email").value;
+    const emailsCadastrados = ["daymendesdev@gmail.com", "daymendesrp@gmail.com", "luam.lc19@gmail.com"];
+    
+    if (emailsCadastrados.includes(email)) { 
+        document.getElementById("exibirErro").innerText = "Há um cadastro com esse e-mail no nosso sistema!";
+        document.getElementById("exibirErro").style.color = "red";
+        return;
+    }
 
     if (senha.value === senha2.value) {
         window.location.href = "opcoes.html";
